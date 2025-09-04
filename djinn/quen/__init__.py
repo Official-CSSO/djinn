19f25527c680e47167d06f79f2bfeb9c08e24df7
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-04 14:31:15 +0800

    init: create mongodb wrapper

--- djinn/quen/__init__.py
+++ djinn/quen/__init__.py
@@ -1,6 +1,18 @@
 # MongoDB wrapper 
 
-class Quen:
-    pass
+from djinn.constants import MONGO_DB
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.database import Database
+from pymongo.collection import Collection
 
+class Quen:
+    motor: AsyncIOMotorClient
+    db: Database
+    user: Collection
+
+    def __init__(self):
+        self.motor = AsyncIOMotorClient(MONGO_DB)
+        self.db = self.motor["djinn"]
+        self.user = self.db["user"]
+    
 
