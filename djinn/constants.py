8a9442e606f20e3e293eaaf4033ac52fb114b415
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-05 13:26:28 +0800

    ref: add motor client

--- djinn/constants.py
+++ djinn/constants.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv; load_dotenv()
 import discord
+from motor.motor_asyncio import AsyncIOMotorClient
 
 TOKEN = os.getenv("TOKEN")
 GUILD = discord.Object(id=1409368851589763195)
 MONGO_DB = os.getenv("MONGODB")
+
+MOTOR = AsyncIOMotorClient(MONGO_DB) 
