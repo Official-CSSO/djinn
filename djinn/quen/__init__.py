b133991526e9f3c5eefc1b858eaad00d781207e8
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-05 13:27:21 +0800

    ref: fix database wrapper

--- djinn/quen/__init__.py
+++ djinn/quen/__init__.py
@@ -1,18 +1,41 @@
 # MongoDB wrapper 
 
-from djinn.constants import MONGO_DB
-from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.database import Database
-from pymongo.collection import Collection
+from typing import Optional
+
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCollection,
+    AsyncIOMotorDatabase,
+)
+
+from .user import User
 
 class Quen:
     motor: AsyncIOMotorClient
-    db: Database
-    user: Collection
+    db: AsyncIOMotorDatabase
+    user: AsyncIOMotorCollection
 
-    def __init__(self):
-        self.motor = AsyncIOMotorClient(MONGO_DB)
+    def __init__(self, motor: AsyncIOMotorClient):
+        self.motor = motor
         self.db = self.motor["djinn"]
         self.user = self.db["user"]
     
+    
+    async def get_user(self, id: int) -> Optional[User]:
+        u = await self.user.find_one({"_id": id})
+        if not u:
+            return None
+        
+        return User(**u)
+
+    async def update_user_bal(self, id: int, amount: int) -> User:
+        u = await self.user.find_one_and_update(
+            {"_id": id},
+            {
+                "$inc": {"bal": amount}
+            },
+            upsert=True,
+            return_document=True
+        )
+        return User(**u)
 
